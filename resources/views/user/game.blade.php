3b63f482bb63dfa38d7fe039fe7292869bfb56de
Gabriel-boiza <gabriel.boiza@inslapineda.cat>
2025-03-28 13:19:58 +0100

    v3

--- resources/views/user/game.blade.php
+++ resources/views/user/game.blade.php
@@ -3,18 +3,24 @@
 @section('content')
 
 
-<div class="flex flex-col items-center  w-full  ">
+<div class="mt-16 gap-16 flex flex-col items-center  w-full  ">
     <div>
         <h2>Instrucciones</h2>
+        <div class="grid grid-cols-3 gap-16">
         <article>
-
+            sda
         </article>
+        <article>
+            sdf
+        </article>
+        <article>
+            sdf
+        </article>
+    </div>
     </div>
     <div>
         <h2>Juego</h2>
         <article>
-
-        
     </div>
 </div>
 
